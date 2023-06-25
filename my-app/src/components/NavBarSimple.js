b67166e45f02ec73bdea475e7251781e7d025351
Fredrik Gustafsson <freddish@hotmail.com>
2023-06-25 13:33:27 +0000

    add NavBarSimple.js /components

---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class NavBarSimple extends Component {
+  render() {
+    return (
+      <div className = {css.NavBar}>
+        <h1>My Gallery</h1>
+        <span></span>
+        <button></button>
+      </div>
+    )
+  }
+}
+
+export default NavBarSimple
