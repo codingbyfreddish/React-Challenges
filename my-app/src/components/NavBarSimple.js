9904ac68b4b2cabb3654b6b3e70ed6e388b87ba7
Fredrik Gustafsson <freddish@hotmail.com>
2023-06-28 09:06:58 +0000

    add completed .js files /components

--- my-app/src/components/NavBarSimple.js
+++ my-app/src/components/NavBarSimple.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import css from "./css/NavBarSimple.module.css";
 
 class NavBarSimple extends Component {
-  render() {
-    return (
-      <div className = {css.NavBar}>
-        <h1>My Gallery</h1>
-        <span></span>
-        <button></button>
-      </div>
-    )
-  }
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            message: "Hello, guest!",
+            buttonText: "Log in"
+        }
+    }
+
+    handleclick = () => {
+        this.setState((prevState) => ({
+            message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+            buttonText: prevState.buttonText === "Log out" ? "Log in" : "Log out",
+        }), ()=> console.log(this.state.message))
+        
+    }
+
+    render() {
+        return (
+            <div className={css.NavBar}>
+                <h1>My Gallery</h1>
+                <div>
+                    <span>{this.state.message}</span>
+                    <button onClick={() => this.handleclick()}>{this.state.buttonText}</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NavBarSimple
